68a9b92e2c82be7eda1e5c8cd6606310a29897d3
Artyom <litovskys@gmail.com>
2019-11-17 13:52:21 +0300

    Updates

---
+++ precache-manifest.ea8611113d94a7e74dce964c03bd9108.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad0f1c6e50c681d5505226d697718e4",
+    "url": "/cities/index.html"
+  },
+  {
+    "revision": "c31f1eb52a606a3b3431",
+    "url": "/cities/static/css/main.355806a8.chunk.css"
+  },
+  {
+    "revision": "ab7f7388f12bee3c8ceb",
+    "url": "/cities/static/js/2.34155f1f.chunk.js"
+  },
+  {
+    "revision": "c31f1eb52a606a3b3431",
+    "url": "/cities/static/js/main.194e4a91.chunk.js"
+  },
+  {
+    "revision": "bc74dccd89eacd301391",
+    "url": "/cities/static/js/runtime-main.3d508eb4.js"
+  }
+]);
